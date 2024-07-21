3a554bb0bddaddcbbf72509f7949cb041972f208
gildembergleite <gildembergleite@gmail.com>
2024-07-21 19:49:05 -0300

    chore: configure host images in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'v2-allugator-images.s3.amazonaws.com',
+        protocol: 'https',
+      },
+      {
+        hostname: 'yacare-products-image.s3.sa-east-1.amazonaws.com',
+        protocol: 'https',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
