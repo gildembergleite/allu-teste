7c1c2c82b74c6748b1b44a7cc2dfc45d2b3acf19
gildembergleite <gildembergleite@gmail.com>
2024-07-22 13:59:54 -0300

    refactor: move logo directory

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,3 @@
-import logoAllu from '@/assets/logo_allu.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Cart } from './cart'
@@ -8,8 +7,19 @@ export function Header(): JSX.Element {
     <header className="bg-background border-b border-muted px-4 md:px-6 py-6 flex justify-center">
       <div className="flex w-full max-w-7xl items-center justify-between">
         <div className="flex items-center gap-12">
-          <Link href="/" className="flex items-center gap-2" prefetch={false}>
-            <Image src={logoAllu} alt="allu" className="max-w-20" />
+          <Link
+            href="/"
+            className="flex w-full max-w-20 items-center gap-2"
+            prefetch={false}
+          >
+            <Image
+              src={'/logo_allu.png'}
+              alt="allu"
+              className="w-full h-full object-contain"
+              width={0}
+              height={0}
+              sizes="100vw"
+            />
           </Link>
           <nav className="hidden md:flex items-center gap-6">
             <Link
